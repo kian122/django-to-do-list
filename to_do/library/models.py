f4ed860ed501f33237f23153ce6a7eebdc01e592
flawlessness-professional <mhrabykyan2@gmail.com>
2023-10-14 11:23:49 -0700

    this is the model for the todo nothing spec

--- to_do/library/models.py
+++ to_do/library/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class To_do(models.Model):
+    name = models.CharField(max_length=80)
+    decription = models.TextField(max_length=800)
+    time = models.TimeField(auto_now_add=True)
