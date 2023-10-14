15748d155a8288069cf886dc9a398d3e85c7a392
flawlessness-professional <mhrabykyan2@gmail.com>
2023-10-14 11:27:02 -0700

    Change the name for better practice

--- to_do/library/models.py
+++ to_do/library/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class To_do(models.Model):
+class Todo(models.Model):
     name = models.CharField(max_length=80)
     decription = models.TextField(max_length=800)
     time = models.TimeField(auto_now_add=True)
