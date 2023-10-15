0bad18898589c6a92fcca3cf34e3ac396b92043a
flawlessness-professional <mhrabykyan2@gmail.com>
2023-10-15 10:39:46 -0700

    creating the model with some tables

--- to_do/library/models.py
+++ to_do/library/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Todo(models.Model):
+    created_by = models.ForeignKey(User , on_delete=models.CASCADE , blank=False , null=True)
     name = models.CharField(max_length=80)
-    decription = models.TextField(max_length=800)
+    description = models.TextField(max_length=800)
     time = models.TimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
