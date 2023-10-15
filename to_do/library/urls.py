e0d0f34659a6e1befb2db98a0da891229d02f7b6
flawlessness-professional <mhrabykyan2@gmail.com>
2023-10-15 10:40:52 -0700

    adding all the important urls

--- to_do/library/urls.py
+++ to_do/library/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, create_index , update_view , delete_index , create_user , error
 
 urlpatterns = [
+    path('create_user' , create_user , name="user_create"),
     path('', index),
+    path('error' , error),
+    path('create', create_index ),
+    path('<str:pk>/delete', delete_index , name="delete"),
+    path('<str:pk>/update', update_view , name="update"),
 ]
